3dca84b232c01c38bea025ae4be803712cc64302
vlogg1527 <118616724+vlogg1527@users.noreply.github.com>
2024-05-10 09:16:01 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -75,6 +75,6 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
